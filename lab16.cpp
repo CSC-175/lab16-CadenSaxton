9be1816ed7d1f4e2087437dfa2b29054d5ec05ee
caden saxton <cadensax@umich.edu>
2024-11-14 12:27:10 -0500

    Commit 1

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,24 @@
-// Code to implement the gcf function goes here
+using namespace std;
+
+int gcf(int a,int b)
+{
+int remainder;
+    if(a > b)
+    {
+    remainder = a%b;
+        if (remainder ==0)
+            return b;
+        else  return gcf(b,remainder);
+    }
+    else if (b > a)
+    {
+        remainder = b%a;
+        if(remainder == 0)
+            return a;
+        else
+            return gcf(a,remainder);
+    }
+
+
+    }
+
